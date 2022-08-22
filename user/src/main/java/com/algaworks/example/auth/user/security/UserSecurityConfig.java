d81d9b62fbb5b9c0f1ecd1749902371ff4c0a8af
Mario Miranda <mario_chan15@hotmail.com>
2022-08-22 11:11:31 -0400

    User > ResourceServer > Customized Token

--- user/src/main/java/com/algaworks/example/auth/user/security/UserSecurityConfig.java
+++ user/src/main/java/com/algaworks/example/auth/user/security/UserSecurityConfig.java
@@ -1,14 +1,20 @@
 package com.algaworks.example.auth.user.security;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -16,15 +22,38 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class UserSecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http,
-            UserDetailsService userDetailsService) throws Exception {
-        return http.userDetailsService(userDetailsService)
-                .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
-                .and()
-                .httpBasic(withDefaults())
-                .build();
-    }
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests().anyRequest().authenticated()
+			.and().csrf().disable()
+				.oauth2ResourceServer()
+				.jwt()
+					.jwtAuthenticationConverter(jwtAuthenticationConverter());
+
+		return http.build();
+	}
+    
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+
+		JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+		converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+			List<String> userRoleAuthorities = jwt.getClaimAsStringList("authorities");
+			
+			if (userRoleAuthorities == null) {
+				return Collections.emptyList();
+			}
+			
+			JwtGrantedAuthoritiesConverter scopesConverter = new JwtGrantedAuthoritiesConverter();
+			
+			Collection<GrantedAuthority> scopeAuthorities = scopesConverter.convert(jwt);
+			
+			scopeAuthorities.addAll(userRoleAuthorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+			
+			return scopeAuthorities;
+		});
+
+		return converter;
+	}
 
 }
