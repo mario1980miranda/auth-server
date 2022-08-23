f6539515bbaaa99a3177fbe0a2d9bccf26ff89f3
Mario Miranda <mario_chan15@hotmail.com>
2022-08-23 11:08:57 -0400

    Backend to backend authentication

--- post/src/main/java/com/algaworks/example/auth/post/api/PostController.java
+++ post/src/main/java/com/algaworks/example/auth/post/api/PostController.java
@@ -1,10 +1,10 @@
 package com.algaworks.example.auth.post.api;
 
 
-import com.algaworks.example.auth.post.client.UserClient;
-import com.algaworks.example.auth.post.domain.Post;
-import com.algaworks.example.auth.post.domain.PostRepository;
-import com.algaworks.example.auth.post.security.SecurityService;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -17,8 +17,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.stream.Collectors;
+import com.algaworks.example.auth.post.client.UserClient;
+import com.algaworks.example.auth.post.domain.Post;
+import com.algaworks.example.auth.post.domain.PostRepository;
+import com.algaworks.example.auth.post.security.CanWritePosts;
+import com.algaworks.example.auth.post.security.SecurityService;
 
 @RestController
 @RequestMapping("/posts")
@@ -48,8 +51,7 @@ public class PostController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	//TODO Implementar segurança
-	//@CanCreatePost
+	@CanWritePosts
 	public PostDetailedResponse create(@RequestBody @Valid PostRequest postRequest) {
 		final Post post = new Post(securityService.getUserId(), postRequest.getTitle(), postRequest.getContent());
 		postRepository.save(post);
